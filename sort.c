9452a40e0446583e2d623df1a032a98e3afa1475
Christopher Bellotti <cbellott@ubuntu-1404.eecis.udel.edu>
2018-03-07 13:13:35 -0500

    still only printing half of the array

--- sort.c
+++ sort.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -70,19 +71,21 @@ int main(int argc, char * argv[]){
   for (int i = 0; i < ARRAY_SIZE; i++) {
     printf("%d, ", values[i]);
   }
+  printf("\n");
   fflush(stdout);
 
-  pthread_create(&sort1_tid, &attr, sort, &left);
-  pthread_create(&sort2_tid, &attr, sort, &right);
+  pthread_create(&sort1_tid, &attr, sort, left);
+  pthread_create(&sort2_tid, &attr, sort, right);
   pthread_join(sort1_tid, NULL);
   pthread_join(sort2_tid, NULL);
-  pthread_create(&merge_tid, &attr, merge, &right);
+  pthread_create(&merge_tid, &attr, merge, right);
   pthread_join(merge_tid, NULL);
 
   printf("FINAL ARRAY: ");
   for (int i = 0; i < ARRAY_SIZE; i++) {
     printf("%d, ", values[i]);
   }
+  printf("\n");
   fflush(stdout);
 
   return 1;
